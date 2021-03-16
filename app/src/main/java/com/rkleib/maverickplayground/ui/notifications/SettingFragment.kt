9bd02877456bf78b844f4ad296451aca13087cd1
Rommy K <rommy.kleib@gmail.com>
2021-03-16 14:30:47 +0700

    add needed library, fix icon and label

--- app/src/main/java/com/rkleib/maverickplayground/ui/notifications/SettingFragment.kt
+++ app/src/main/java/com/rkleib/maverickplayground/ui/notifications/SettingFragment.kt
@@ -10,9 +10,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.rkleib.maverickplayground.R
 
-class NotificationsFragment : Fragment() {
+class SettingFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var notificationsViewModel: SettingViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -20,8 +20,8 @@ class NotificationsFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+                ViewModelProvider(this).get(SettingViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_setting, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
         notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
