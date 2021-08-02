4c717bb374eac0acbfb9e72d729ab6f67f858f02
Rommy K <rommy.kleib@gmail.com>
2021-08-02 15:05:14 +0700

    add fragment view binding delegate

--- app/src/main/java/com/rkleib/maverickplayground/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/rkleib/maverickplayground/ui/dashboard/DashboardFragment.kt
@@ -1,29 +1,19 @@
 package com.rkleib.maverickplayground.ui.dashboard
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.lifecycle.MutableLiveData
 import com.airbnb.mvrx.MavericksState
-import com.airbnb.mvrx.activityViewModel
 import com.airbnb.mvrx.fragmentViewModel
 import com.airbnb.mvrx.withState
 import com.rkleib.maverickplayground.BaseFragment
 import com.rkleib.maverickplayground.databinding.FragmentDashboardBinding
-import retrofit2.Response
+import com.rkleib.maverickplayground.ui.viewBinding
 
 data class DashboardState(val count: Int = 0) : MavericksState
 
 class DashboardFragment : BaseFragment() {
-    private var _binding: FragmentDashboardBinding? = null
-    private val binding get() = _binding!!
+    private val binding: FragmentDashboardBinding by viewBinding()
     private val viewModel: DashboardViewModel by fragmentViewModel()
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.btnCount.setOnClickListener {
@@ -34,6 +24,4 @@ class DashboardFragment : BaseFragment() {
     override fun invalidate() = withState(viewModel) { state ->
         binding.textDashboard.text = state.count.toString()
     }
-
-
 }
